6de4e469c7c716ca8933b3affe695ed098a1e485
Fatih ŞENER <48569471+FatihSener@users.noreply.github.com>
2019-03-30 17:01:06 +0300

    Initial commit

---
+++ Köstebek Oyunu/FormKostebekOyunu.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Köstebek_Oyunu
+{
+    partial class frmKostebekOyunu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flpOyunEkranı = new System.Windows.Forms.FlowLayoutPanel();
+            this.tmrKontrol = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // flpOyunEkranı
+            // 
+            this.flpOyunEkranı.BackColor = System.Drawing.Color.White;
+            this.flpOyunEkranı.Location = new System.Drawing.Point(1, 0);
+            this.flpOyunEkranı.Name = "flpOyunEkranı";
+            this.flpOyunEkranı.Size = new System.Drawing.Size(791, 348);
+            this.flpOyunEkranı.TabIndex = 0;
+            // 
+            // tmrKontrol
+            // 
+            this.tmrKontrol.Interval = 1000;
+            this.tmrKontrol.Tick += new System.EventHandler(this.tmrKontrol_Tick);
+            // 
+            // frmKostebekOyunu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(795, 350);
+            this.Controls.Add(this.flpOyunEkranı);
+            this.Name = "frmKostebekOyunu";
+            this.Text = "Köstebek Oyunu";
+            this.Load += new System.EventHandler(this.frmKostebekOyunu_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flpOyunEkranı;
+        private System.Windows.Forms.Timer tmrKontrol;
+    }
+}
+
---
+++ Köstebek Oyunu/FormKostebekOyunu.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Köstebek_Oyunu
+{
+    public partial class frmKostebekOyunu : Form
+    {
+        public frmKostebekOyunu()
+        {
+            InitializeComponent();
+        }
+        int puan = 0;
+        Random random = new Random();
+
+        private void frmKostebekOyunu_Load(object sender, EventArgs e)
+        {
+            tmrKontrol.Start();
+            for (int i = 1; i < 85; i++)
+            {
+                Button button = new Button();
+                button.BackColor = Color.White;
+                button.ForeColor = Color.Black;
+                button.Width = 50;
+                button.Height = 50;
+                button.Text = i.ToString();
+                flpOyunEkranı.Controls.Add(button);
+            }
+        }
+
+        private void tmrKontrol_Tick(object sender, EventArgs e)
+        {
+            int rastgele = random.Next(1, 85);
+
+            foreach (Button button in flpOyunEkranı.Controls)
+            {
+                //Button button = button as Button;
+                if (button.Text == rastgele.ToString())
+                {
+                    //top.ImageLocation = Application.StartupPath + @"\..\..\Resources\top.png";
+                    //button.BackgroundImage = Image.FromFile("foto0.png");
+                    button.BackColor = Color.Red;
+
+                    button.Click += Button_Click;
+                }
+                else
+                {
+                    button.BackColor = Color.White;
+                }
+
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (button.BackColor == Color.Red)
+            {
+                puan++;
+                button.BackColor = Color.Green;
+            }
+
+            //if (button.BackgroundImage == Image.FromFile("foto0.png"))
+            //{
+            //    puan++;
+            //   button.BackgroundImage = Image.FromFile("foto22.png");
+            //}
+            if (button.BackColor == Color.White)
+            {
+                puan--;
+            }
+
+
+            this.Text = puan.ToString();
+        }
+    }
+}
